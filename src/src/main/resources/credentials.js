0940d5c1e762375e22a58a9ab3b8c472d49a5d83
AdrianCes <aceslopez@danielcastelao.org>
2022-06-17 11:31:01 +0200

    Hacemos un refactor de Quickstart y implementamos dependencias en gradler

--- src/src/main/resources/credentials.js
+++
@@ -1,1 +0,0 @@
-{"web":{"client_id":"928963884762-0pun7b50najc56t7ic3u60s61t4r3ufg.apps.googleusercontent.com","project_id":"drive-api-353606","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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"}}
